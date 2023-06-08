6a60502bb13c35bee59ebbb766c417d926f4d2c7
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-08 11:09:37 +0200

    add

---
+++ src/main/java/JAVAU2W2D4THEORY/JavaU2W2D4TheoryApplication.java
@@ -0,0 +1,13 @@
+package JAVAU2W2D4THEORY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaU2W2D4TheoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaU2W2D4TheoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JAVAU2W2D4THEORY/JavaU2W2D4TheoryApplicationTests.java
@@ -0,0 +1,13 @@
+package JAVAU2W2D4THEORY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaU2W2D4TheoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
